2d35d69d4d67b26c6107807b96f9a1f61451a481
rafa-9 <rafa.inspired9@gmail.com>
2023-10-06 17:08:20 -0400

    remove wrong sym

--- src/download_model.py
+++ src/download_model.py
@@ -7,8 +7,6 @@ MODEL_NAME = os.environ.get("MODEL_NAME", "TheBloke/Genz-70b-AWQ")
 MODEL_REVISION = os.environ.get("MODEL_REVISION", "main")
 MODEL_BASE_PATH = os.environ.get("MODEL_BASE_PATH", "/runpod-volume/")
 
-# Symlinking files from Network Volume
-ln -s "/runpod-volume/Genz-70b-AWQ" "/Genz-70b-AWQ"
 
 def download_model():
     # Download the model from hugging face
